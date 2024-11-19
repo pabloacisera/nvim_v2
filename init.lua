1070c6696f589a1c0d4abe2818ee9e6a40ef19fa
pabloacisera <pabloacisera2021@gmail.com>
2024-11-18 21:35:16 -0300

    commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+vim.g.mapleader=" "
+
+vim.opt.number=true
+vim.opt.tabstop=2
+vim.opt.shiftwidth=2
+vim.opt.autoindent=true
+
+
+require "plugins"
---
+++ lua/plugins.lua
@@ -0,0 +1,23 @@
+vim.cmd([[
+	augroup packer_user_config
+	autocmd!
+	autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+     augroup end
+]])
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use {
+  		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		-- or                            , branch = '0.1.x',
+  		requires = { {'nvim-lua/plenary.nvim'} }
+		}
+	
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+			}
+	end
+)
